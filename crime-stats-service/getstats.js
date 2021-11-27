cec0d19cff1b7ab3546c818e4ccd9980fa1c9a25
mehavira <virapanm@oregonstate.edu>
2021-11-27 16:37:57 -0700

    Changed directory of /css

--- crime-stats-service/getstats.js
+++ crime-stats-service/getstats.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/stats/', function(req, res){
     ////ACCEPT AND STORE INPUTS
